5188c6302e56c9af23b35d5f1272e12124df88fc
Freddie Heath <fheath090401@gmail.com>
2022-09-30 02:14:57 +0100

    modified:   freddieheath.css modified:   freddieheath.html modified:   freddieheath.js

--- freddieheath.js
+++ freddieheath.js
@@ -1,8 +1,9 @@
 function myFunction() {
     document.getElementById("about-me").style.display = "block";
-    document.getElementById("about-me").style.margin = "10px";
     document.getElementById("about-me").style.fontSize = "20px";
     document.getElementById("about-me").style.height = "259px";
+    document.getElementById("about-me").style.lineHeight = "35px";
+    document.getElementById("about-me").style.margin = "10px";
     document.getElementById("title-text").style.display = "none";
 }
 
