811d93ab2891d193d2d2d831704463e8d7350066
Freddie Heath <fheath090401@gmail.com>
2022-09-30 03:24:55 +0100

    modified:   freddieheath.css modified:   freddieheath.html 	modified:   freddieheath.js modified:   mtp.html

--- freddieheath.js
+++ freddieheath.js
@@ -1,13 +1,26 @@
-function myFunction() {
-    document.getElementById("about-me").style.display = "block";
-    document.getElementById("about-me").style.fontSize = "20px";
-    document.getElementById("about-me").style.height = "259px";
-    document.getElementById("about-me").style.lineHeight = "35px";
-    document.getElementById("about-me").style.margin = "10px";
+function mouseLeave() {
+    document.getElementById("title-text").style.display = "block";
+    document.getElementById("intro").style.display = "none";
+
+}
+
+function intro() {
+    document.getElementById("intro").style.display = "block";
+    document.getElementById("intro").style.fontSize = "20px";
+    document.getElementById("intro").style.height = "259px";
+    document.getElementById("intro").style.lineHeight = "35px";
+    document.getElementById("intro").style.margin = "10px";
     document.getElementById("title-text").style.display = "none";
 }
 
-
+function mtpIntro() {
+    document.getElementById("mtp-intro").style.display = "block";
+    document.getElementById("mtp-intro").style.fontSize = "20px";
+    document.getElementById("mtp-intro").style.height = "259px";
+    document.getElementById("mtp-intro").style.lineHeight = "35px";
+    document.getElementById("mtp-intro").style.margin = "10px";
+    document.getElementById("title-text").style.display = "none";
+}
 
 
 /* 
